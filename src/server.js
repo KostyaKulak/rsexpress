4e66c35978122c945d538d6b2904d331838e88d3
Kanstantsin Kulak <kkulak24@gmail.com>
2020-10-26 21:27:05 +0300

    feat: add db connection on start

--- src/server.js
+++ src/server.js
@@ -1,6 +1,12 @@
 const { PORT } = require('./common/config');
 const app = require('./app');
+const { logger } = require('./common/logging/logger');
+
+if (process.env.STORAGE_MODE === 'db') {
+  const { connect } = require('./db/db.client');
+  connect();
+}
 
 app.listen(PORT, () =>
-  console.log(`App is running on http://localhost:${PORT}`)
+  logger.info(`App is running on http://localhost:${PORT}`)
 );
