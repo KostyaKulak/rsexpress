b46c46e29b788601f3171e2d6bb1796c3830487f
Kanstantsin Kulak <kkulak24@gmail.com>
2020-10-26 22:14:11 +0300

    feat: update github actions

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ const { PORT } = require('./common/config');
 const app = require('./app');
 const { logger } = require('./common/logging/logger');
 
+logger.info(`Storage mode is ${process.env.STORAGE_MODE}`);
+
 if (process.env.STORAGE_MODE === 'db') {
   const { connect } = require('./db/db.client');
   connect();
