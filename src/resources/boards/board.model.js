ba569f4aead43250db248cf80b742448b15b55d4
Kanstantsin Kulak <kkulak24@gmail.com>
2020-10-26 21:27:44 +0300

    feat: update models

--- src/resources/boards/board.model.js
+++ src/resources/boards/board.model.js
@@ -1,11 +1,34 @@
 const uuid = require('uuid');
+const mongoose = require('mongoose');
 
-class Board {
-  constructor({ id = uuid(), title = 'default title', columns = [] }) {
-    this.id = id;
+class BoardMemory {
+  constructor({ title = 'default title', columns = [] }) {
+    this.id = uuid();
     this.title = title;
     this.columns = columns;
   }
+
+  static toResponse(board) {
+    return board;
+  }
 }
+
+const boardSchema = new mongoose.Schema(
+  {
+    title: String,
+    columns: Array,
+    _id: { type: String, default: uuid }
+  },
+  { versionKey: false }
+);
+
+boardSchema.statics.toResponse = board => {
+  const { title, columns } = board;
+  return { id: board._id, title, columns };
+};
+
+const BoardDB = mongoose.model('Board', boardSchema);
+
+const Board = process.env.STORAGE_MODE === 'memory' ? BoardMemory : BoardDB;
 
 module.exports = Board;
--- src/resources/users/user.model.js
+++ src/resources/users/user.model.js
@@ -1,13 +1,14 @@
 const uuid = require('uuid');
+const mongoose = require('mongoose');
 
-class User {
+class UserMemory {
   constructor({
     id = uuid(),
     name = 'USER',
     login = 'user',
     password = 'P@55w0rd'
   } = {}) {
-    this.id = id;
+    this.id = id || uuid();
     this.name = name;
     this.login = login;
     this.password = password;
@@ -18,5 +19,24 @@ class User {
     return { id, name, login };
   }
 }
+
+const userSchema = new mongoose.Schema(
+  {
+    name: String,
+    login: String,
+    password: String,
+    _id: { type: String, default: uuid }
+  },
+  { versionKey: false }
+);
+
+userSchema.statics.toResponse = user => {
+  const { id, name, login } = user;
+  return { id, name, login };
+};
+
+const UserDB = mongoose.model('User', userSchema);
+
+const User = process.env.STORAGE_MODE === 'memory' ? UserMemory : UserDB;
 
 module.exports = User;
