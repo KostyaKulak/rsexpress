4fec3deaa7fb036a492c69da18c01f020e5fa7dd
Kanstantsin Kulak <kkulak24@gmail.com>
2020-11-02 14:59:46 +0300

    feat: update db start, add admin creation

--- src/db/db.client.js
+++ src/db/db.client.js
@@ -1,14 +1,23 @@
 const mongoose = require('mongoose');
 const { logger } = require('../common/logging/logger');
+const userService = require('../resources/users/user.service');
 
-const connect = () => {
+const connect = runServer => {
   mongoose.connect(process.env.MONGO_CONNECTION_STRING, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   });
   const db = mongoose.connection;
   db.on('error', () => logger.error('Connection error'));
-  db.once('open', () => logger.info('Connected to DB'));
+  db.once('open', () => {
+    logger.info('Connected to DB');
+    runServer();
+    userService.addUser({
+      name: 'admin',
+      login: 'admin',
+      password: 'admin'
+    });
+  });
 };
 
 module.exports = { connect };
