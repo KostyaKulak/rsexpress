b42e9d1e334bdde3485fc2bb797d9ca6db8d0a77
Kanstantsin Kulak <kkulak24@gmail.com>
2020-11-02 14:58:32 +0300

    feat: add auth methods

--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -3,10 +3,20 @@ const usersRepo =
     ? require('./user.memory.repository')
     : require('./user.db.repository');
 const taskService = require('../tasks/task.service');
-
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const { throw401 } = require('../../utils/error.utils');
+const { catchErrors } = require('../../utils/error.handler');
+const saltRounds = 10;
+// eslint-disable-next-line no-sync
+const salt = bcrypt.genSaltSync(saltRounds);
 const getAll = () => usersRepo.getAll();
 const getUser = id => usersRepo.getUser(id);
-const addUser = user => usersRepo.addUser(user);
+const addUser = user => {
+  // eslint-disable-next-line no-sync
+  user.password = bcrypt.hashSync(user.password, salt);
+  return usersRepo.addUser(user);
+};
 const updateUser = (id, user) => usersRepo.updateUser(id, user);
 const deleteUser = async id => {
   if (await usersRepo.deleteUser(id)) {
@@ -15,5 +25,44 @@ const deleteUser = async id => {
   }
   return false;
 };
+const getToken = async (login, password) => {
+  if (!login || !password) {
+    return null;
+  }
+  // eslint-disable-next-line no-sync
+  password = bcrypt.hashSync(password, salt);
+  const user = await usersRepo.getPasswordByUser(login, password);
+  if (user) {
+    const id = user._id ? user._id : user.id;
+    const payload = { sub: id, login };
+    const token = jwt.sign(payload, process.env.JWT_SECRET_KEY, {
+      expiresIn: 10000
+    });
+    return { token };
+  }
+};
 
-module.exports = { getAll, getUser, updateUser, addUser, deleteUser };
+const checkToken = catchErrors(async (req, res, next) => {
+  if (req.headers.authorization) {
+    const token = req.headers.authorization.split(' ')[1];
+    jwt.verify(token, process.env.JWT_SECRET_KEY, err => {
+      if (err) {
+        throw401();
+      } else {
+        return next();
+      }
+    });
+  } else {
+    throw401();
+  }
+});
+
+module.exports = {
+  getAll,
+  getUser,
+  updateUser,
+  addUser,
+  deleteUser,
+  getToken,
+  checkToken
+};
